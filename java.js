db8db20c4df902c3bb2d97b5589f3a8dbc50cae0
jezrreloriginal <124934158+jezrreloriginal@users.noreply.github.com>
2023-08-19 18:18:26 -0300

    d

---
+++ java.js
@@ -0,0 +1,15 @@
+// Defina o tempo de espera em milissegundos
+var tempoDeEspera = 5000; // 5 segundos
+
+// Função para redirecionar para outra página
+function redirecionar() {
+    window.location.href = "outra_pagina.html";
+}
+
+// Função para iniciar a animação e o redirecionamento
+function iniciarAnimacao() {
+    setTimeout(redirecionar, tempoDeEspera);
+}
+
+// Chame a função para iniciar a animação quando a página for carregada
+window.onload = iniciarAnimacao;
